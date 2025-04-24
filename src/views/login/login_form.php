d84b8f9c72080fd49c83f96e11682affdd59f40c
Samuel Frederico <sfrederico@ucs.br>
2025-04-24 12:43:10 -0300

    feat: :sparkles: add create account functionality with dynamic fields for user roles

--- src/views/login/login_form.php
+++ src/views/login/login_form.php
@@ -1,3 +1,6 @@
+<?php
+require_once __DIR__ . '/../comum/header.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,5 +19,6 @@
         <br>
         <button type="submit">Login</button>
     </form>
+    <p>Don't have an account? <a href="../create_account.php">Create one here</a>.</p>
 </body>
 </html>
